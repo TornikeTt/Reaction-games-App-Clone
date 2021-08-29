62acda083c1c9080929cad843b1e98b2514214ca
TornikeTt <chargeishvilitornike@gmail.com>
2021-08-29 04:11:32 +0400

    separation non starting and starting game components

--- src/Components/ColorChangeGame.js
+++ src/Components/ColorChangeGame.js
@@ -4,30 +4,30 @@ import {useState} from "react"
 const ColorChangeGame = () => { 
     const [ Starting_ColorChange , setStarting_ColorChange ] = useState(false)
 
-    const startGame_ColorChange = () => { 
+    const startGame_ColorChange_Hendler = () => { 
         setStarting_ColorChange(true);
     }
 
-    if (  Starting_ColorChange === false ) { 
-        return ( 
-            <div className="ColorChange_Game">
-                <div className="HowToPlye">
-                    <p>click on the frame as soon as the color 
-                        changes, as soon as possible </p>
-                </div>
+    return ( 
+        <div className="ColorChange_Game">
+            <div className="HowToPlye">
+                <p>click on the frame as soon as the color 
+                    changes, as soon as possible </p>
+            </div>
 
-                <div className="start_game" onClick={startGame_ColorChange}>
-                    <p> start </p>
-                </div>
+            <div 
+                className="start_game" 
+                onClick={startGame_ColorChange_Hendler} >
+                { 
+                    Starting_ColorChange ? 
+                        <div> game is starting </div>
+                    :
+                        <p> start </p> 
+                }
             </div>
-        )
-    }  else { 
-        return ( 
-            <div>
-                hello
-            </div>
-        )
-    } 
+
+        </div>
+    )
     
 }
 
