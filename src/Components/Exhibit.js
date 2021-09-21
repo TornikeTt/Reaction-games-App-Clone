333d5463662cbdb415683abef359fd90f91b1231
TornikeTt <chargeishvilitornike@gmail.com>
2021-09-21 22:53:09 +0400

    add instagramm icone and functional

--- src/Components/Exhibit.js
+++ src/Components/Exhibit.js
@@ -2,81 +2,110 @@ import React from 'react';
 import { useState  } from "react"
 import WhatShowYou from "./WhatShowYou"
 
-const Exhibit = () => { 
+const Exhibit = () => {
   const [ volume, setVolume ] = useState(true);
-  const [ settingToggle, setSettingToggle ] = useState(false); 
+  /*
+    1. inside Menu "settingToggle" is false
+    2. inside Setting "settingToggle" is true
+  */
+  const [ settingToggle, setSettingToggle ] = useState(false);
+
   const [ difficultyValue , setDifficultyValue ] = useState(5);
   const [ GameIdentification, setGameIdentification ] = useState(0)
 
-  const Volume_Hendler = () => { 
+  const Volume_Hendler = () => {
     setVolume(!volume)
   }
 
-  const Setting_Hendler = () => { 
+  const Setting_Hendler = () => {
     setSettingToggle(!settingToggle);
   }
 
-  const Header_Name_Hendler = () => { 
-    if ( settingToggle === true && GameIdentification === 0) { 
+  const Header_Name_Hendler = () => {
+    if ( settingToggle === true && GameIdentification === 0) {
+      // when user is inside Settings
         return "SETTINGS"
-    } else if ( settingToggle === false && GameIdentification === 0 ) { 
+    } else if ( settingToggle === false && GameIdentification === 0 ) {
+      // when user is inside Menu
         return "MENU"
-    } else if ( GameIdentification > 0) { 
-      switch(GameIdentification) { 
+    } else if ( GameIdentification > 0) {
+      // when user Click Any game
+      switch(GameIdentification) {
           case 1:
               return "COLOR CHANGE"
               break;
-          case 2: 
+          case 2:
               return "FIND NUMBER"
               break;
-          case 3: 
+          case 3:
               return "CATCH THE BALL"
               break;
-          case 4: 
+          case 4:
               return "FIND COLOR"
               break;
-          default: 
+          default:
               return "COMING SOON..."
       };
     }
   }
 
-  const left_icone_hendler = () => { 
-    if( GameIdentification > 0 ) { 
-      // if we click any game
+  const left_icone_hendler = () => {
+    if( GameIdentification > 0 || settingToggle === true) {
+      // if we click any game or if we are inside Settings
+      // show X icone left side of header tag
       return "icones/times-solid.svg"
     }
 
-    if( settingToggle ) { 
-        //inside setting 
-        return "icones/times-solid.svg"
-    } else if( settingToggle === false ) { 
+    if( settingToggle === false ) {
         //inside Menu
-        if( volume ) { 
-            return "icones/volume-up-solid.svg" 
-        } else { 
+        if( volume ) {
+            return "icones/volume-up-solid.svg"
+        } else {
             return  "icones/volume-mute-solid.svg"
         }
     }
   }
 
-  const x = () => { 
-    // when we be in a game and click X icone it's go back to the menu
-    setGameIdentification(0)        
+  const right_icone_hendler = () => {
+      
+    if( GameIdentification > 0) {
+        return { 
+            click: "",
+            icone: "icones/sync-alt-solid.svg",
+        }
+    } else {
+        if( settingToggle === true && GameIdentification <= 0) { 
+            return { 
+                click: "document.location='facebook.com'",
+                icone: "icones/instagram-brands.svg",
+            }
+        } else { 
+            return { 
+                click: () => Setting_Hendler(),
+                icone: "icones/cog-solid.svg",
+            }
+        }
+    }
+      
   }
 
-  return ( 
+  const x = () => {
+    // when we be in a game and click X icone it's go back to the menu
+    setGameIdentification(0)
+  }
+
+  return (
     <main>
         <header>
             <img
                 src = {
                     left_icone_hendler()
                 }
-                alt="Speakers icone" 
+                alt="Speakers icone"
                 className="icone"
-                onClick = { 
+                onClick = {
                     GameIdentification > 0 ? x :
-                    settingToggle ? Setting_Hendler : Volume_Hendler 
+                    settingToggle ? Setting_Hendler : Volume_Hendler
                 }
             />
 
@@ -86,20 +115,20 @@ const Exhibit = () => {
 
             { /*  here onClick string has string which
                 gave us error change another time  */ }
-            <img 
-                src={ GameIdentification > 0 ? 
-                        "icones/sync-alt-solid.svg" :
-                        "icones/cog-solid.svg" }
-                alt="settings icone"
-                className="icone"
-                onClick = { GameIdentification > 0 ? "us" : Setting_Hendler }
-            />
+            <a href={ right_icone_hendler().click } target="_blank">
+                <img
+                    src={ right_icone_hendler().icone }
+                    alt="settings icone"
+                    className="icone"
+                    onClick = { right_icone_hendler().click }
+                />
+            </a>
         </header>
 
-        <div className="shape"> 
-            <WhatShowYou 
+        <div className="shape">
+            <WhatShowYou
                settingToggle = { settingToggle }
-               difficultyValue = { difficultyValue } 
+               difficultyValue = { difficultyValue }
                setDifficultyValue = { setDifficultyValue }
                GameIdentification = { GameIdentification }
                setGameIdentification = { setGameIdentification }
