51523ccc5623c467910ef8ab4bccbeea8a42ecb6
TornikeTt <chargeishvilitornike@gmail.com>
2021-10-26 10:32:32 +0400

    add some description comments

--- src/App.js
+++ src/App.js
@@ -1,37 +1,49 @@
 import './App.scss';
 import Exhibit from "./Components/Exhibit/Exhibit";
-import React , { useState } from "react"
-import { useSpring, animated } from "react-spring"
+import React , { useState } from "react";
+import { useSpring, animated } from "react-spring";
 import "./HelveticaNeueCyr/HelveticaNeueCyr.ttf";
 
 function App() {
-  const [ showNumber , setShowNumber ] = useState(true)
-  const [ Theme , setTheme] = useState(true) // for chaneing Them 
+    /*
+        "showNumber" hook we use to show <Exhibit /> Component 
+        if showNumber is true that means we see animation on the screen
+        if showNumber is false that means we see <Exhibit /> component 
+        on the screen 
+    */
+    const [ showNumber , setShowNumber ] = useState(true)
+    const [ Theme , setTheme] = useState(true) // for change Them 
+    
+    // for all page starting animation 3 , 2 , 1 go.....
+    const count = useSpring({
+        from: { number: 4 },
+        to: { number: 0 },
+        config: { 
+            duration: 2000
+        },
+        /*
+            after animation is finish then onRest props change 
+            "showNumber" hook's state from true to false 
+            which means we see <Exhibit /> component on the screen.
+        */
+        onRest: () => setShowNumber(!showNumber),
+    })
 
-  const count = useSpring({
-    from: { number: 4 },
-    to: { number: 0 },
-    config: { 
-      duration: 2000
-    },
-    onRest: () => setShowNumber(!showNumber),
-  })
-
-  return (
-      <div className={`App ${Theme ? "" : "BlackBackgroundColor"}`}>
-      { 
-        showNumber ? 
-          <div className="number"> 
-            <animated.p>
-            { count.number.to(val => Math.floor(val)) }
-            </animated.p>
-            <small> This is <q>Reaction game</q> Clone </small>
-          </div> 
-        : 
-          <Exhibit Theme = {Theme} setTheme={setTheme}/> 
-      }
-    </div>
-  );
+    return (
+        <div className={`App ${Theme ? "" : "BlackBackgroundColor"}`}>
+            { 
+            showNumber ? 
+            <div className="number"> 
+                <animated.p>
+                { count.number.to(val => Math.floor(val)) }
+                </animated.p>
+                <small> This is <q>Reaction game</q> Clone </small>
+            </div> 
+            : 
+            <Exhibit Theme = {Theme} setTheme={setTheme}/> 
+            }
+        </div>
+    );
 
 }
 
