dc7ae1cd340f3347de3d891347407557cef4a5b8
TornikeTt <chargeishvilitornike@gmail.com>
2021-10-27 09:33:07 +0400

    add some props

--- src/Components/Exhibit/WhatShowYou.js
+++ src/Components/Exhibit/WhatShowYou.js
@@ -5,72 +5,82 @@ import { useSpring, animated } from "react-spring";
 import Games from "../Games/Games"
 
 const WhatShowYou = (props) => {
+    const {
+        settingToggle,
+        difficultyValue,
+        setDifficultyValue,
+        PageIdentification,
+        setPageIdentification,
+        Theme,
+        setTheme,
+        eachLevel,
+        setEachLevel,
+        timeTrack,
+        setTimeTrack,
+    } = props
 
-  const {
-    settingToggle,
-    difficultyValue,
-    setDifficultyValue,
-    PageIdentification,
-    setPageIdentification,
-    Theme,
-    setTheme,
-    eachLevel,
-    setEachLevel
-  } = props
+    /*
+        here we use React spring to have animation
+        animation heppend when react commpoennt change from Menu to Setting
+    */
+    const Setting_animation = useSpring({
+        marginLeft: settingToggle ? 0 : 600,
+    })
 
-  // here we use React spring to have animation
-  // animation heppend when react commpoennt change from Menu to Setting
-  const Setting_animation = useSpring({
-    marginLeft: settingToggle ? 0 : 600
-  })
+    const Menu_animation = useSpring( {
+        marginRight: settingToggle ? 600 : 0
+    })
 
-  const Menu_animation = useSpring( {
-    marginRight: settingToggle ? 600 : 0
-  })
+   
+    if( PageIdentification === 0 ) {
+        // we are inside Menu or Setting
 
-
-  if( PageIdentification === 0 ) {
-      if(settingToggle) {
-          // when user inside Setting
-          return (
-              <animated.div style={Setting_animation}>
-              <Settings
-                  settingToggle = { settingToggle }
-                  difficultyValue = { difficultyValue }
-                  setDifficultyValue = { setDifficultyValue }
-                  Theme = { Theme }
-                  setTheme = { setTheme }
-              />
-              </animated.div>
-          );
-      } else {
-          return (
-              // return inside Menu where we see games
-              gameListData.map( (eachData) => {
-                return (
-                    <animated.div key={ eachData.id} style={Menu_animation}>
-                      <Menu
-                        gameName = { eachData.gameName }
-                        summary = { eachData.summary }
-                        gameNumber = { eachData.id }
-                        setPageIdentification = { setPageIdentification }
+        if(settingToggle) {
+            // when user inside Setting
+            return (
+                <animated.div style={Setting_animation}>
+                    <Settings
+                        settingToggle = { settingToggle }
+                        difficultyValue = { difficultyValue }
+                        setDifficultyValue = { setDifficultyValue }
                         Theme = { Theme }
-                      />
-                    </animated.div>
-                )
-              })
-          )
-      }
-  }  else   {
-      return (
-          <Games 
-              PageIdentification = { PageIdentification }
-              Theme = { Theme }
-              eachLevel = { eachLevel }
-              setEachLevel = { setEachLevel }
-          />
-      );
-  } 
+                        setTheme = { setTheme }
+                    />
+                </animated.div>
+            );
+        } else {
+            return (
+                // return inside Menu where we see games
+                gameListData.map( (eachData) => {
+                    return (
+                        <animated.div key={ eachData.id} style={Menu_animation}>
+                            <Menu
+                                gameName = { eachData.gameName }
+                                summary = { eachData.summary }
+                                gameNumber = { eachData.id }
+                                setPageIdentification = { setPageIdentification }
+                                Theme = { Theme }
+                            />
+                        </animated.div>
+                    )
+                })
+            )
+        }
+    } else {
+        // we are inside in one of the games 
+        return (
+              <Games 
+                PageIdentification = { PageIdentification }
+                Theme = { Theme }
+                eachLevel = { eachLevel }
+                setEachLevel = { setEachLevel }
+                difficultyValue = { difficultyValue }
+                setPageIdentification = { setPageIdentification }
+                timeTrack = { timeTrack }
+                setTimeTrack = { setTimeTrack }
+              />
+        );
+    } 
 }
 
 
