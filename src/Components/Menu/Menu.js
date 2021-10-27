ea7e3be74a5bbca831610be8fd14a21f74b5cf01
TornikeTt <chargeishvilitornike@gmail.com>
2021-10-27 10:02:02 +0400

    add comments

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,51 +1,71 @@
 const Menu = (props) => { 
     const { 
-      gameName, 
-      summary, 
-      gameNumber,
-      setPageIdentification,
-      Theme,
+        gameName, 
+        summary, 
+        gameNumber,
+        setPageIdentification,
+        Theme,
     } = props
 
+    /* 
+        Page_Identification_Hendler function we use to defain which game 
+        user clicked
+    */
     const Page_Identification_Hendler = (gameNumber) => { 
-      setPageIdentification(gameNumber)
+        // hear setPageIdentification hook we use to show relevant game on screen
+        setPageIdentification(gameNumber)
     }
     
     return ( 
         <div 
             className={`Menu ${Theme? "" : "blackThemeBorder"} `}
+            /* 
+                Page_Identification_Hendler function we use to defain which game 
+                user clicked 
+            */
             onClick={ () =>  Page_Identification_Hendler(gameNumber) } > 
-          <h1 className={`game_Name ${Theme? "":"BlackThemeText"}`}>
-            { gameName } 
-          </h1>
 
-          <div className="summary_and_icons">
-              <p className={`summary ${Theme? "" : "BlackThemeText"}`}> 
-                  { summary } 
-              </p>
-              <div>
-                  <img 
-                      src= { 
-                          Theme? 
-                              "icones/question-solid.svg" 
-                              :
-                              "ChnageThemeIcones/question-solid.svg"
-                      }
-                      alt="question icone"
-                      className="icone"
-                  />
-                  <img  
-                      src= { Theme? 
-                              "icones/history-solid.svg" 
-                              :
-                              "ChnageThemeIcones/history-solid.svg"
-                      }
-                      alt="history icone"
-                      className="icone"
-                  />
-              </div>
-          </div>
-      </div>
+            <h1 className={`game_Name ${Theme? "":"BlackThemeText"}`}>
+                { gameName } 
+            </h1>
+
+            <div className="summary_and_icons">
+                <p className={`summary ${Theme? "" : "BlackThemeText"}`}> 
+                    { summary } 
+                </p>
+                <div>
+                    <img 
+                        /*
+                            Here we test that user change Theme or not
+                            if change then we return same icone but 
+                            different svg colors
+                        */
+                        src= { 
+                            Theme? 
+                                "icones/question-solid.svg" 
+                                :
+                                "ChnageThemeIcones/question-solid.svg"
+                        }
+                        alt="question icone"
+                        className="icone"
+                    />
+                    <img  
+                        /*
+                            Here we test that user change Theme or not
+                            if change then we return same icone but 
+                            different svg colors
+                        */
+                        src= { Theme? 
+                                "icones/history-solid.svg" 
+                                :
+                                "ChnageThemeIcones/history-solid.svg"
+                        }
+                        alt="history icone"
+                        className="icone"
+                    />
+                </div>
+            </div>
+        </div>
     );
 }
 
